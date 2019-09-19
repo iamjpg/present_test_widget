f7b4599a8b7356414539f131e2cbe5c23b70f298
JP Given <jgiven@gmail.com>
2019-09-18 19:28:59 -0700

    Initial commit + widget work.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,84 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div id="johnnyonit">
+  <div id="joi-tagline">For home service solutions in real-time<br />
+    Download Johnny On It:<br />
+    <br />
+    <img src="https://www.qr-code-generator.com/wp-content/themes/qr/new_structure/markets/core_market_full/generator/dist/generator/assets/images/websiteQRCode_noFrame.png" width="200" /></div>
+
+  <table id="joi-agent">
+    <tbody>
+      <tr>
+        <td>
+          <div :style="returnAgentImage()" id="joi-agent-image">&nbsp;</div>
+        </td>
+        <td>{{ displayName }}<br />
+          <span v-if="title !== ''">{{ title }} / </span>{{ officeLegalName }}</td>
+      </tr>
+    </tbody>
+  </table>
+  <div id="joi-logo"><img src="https://www.jonnyonit.com/wp-content/uploads/2018/11/JOI_Logo.svg" width="200" /></div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return window.MoxiData.presentation.agentData[0]
+  },
+  methods: {
+    returnAgentImage: function() {
+      return 'background-image:url(' + this.photo + ')';
+    },
+    returnPhone: function() {
+      if (this.mobilePhone) {
+        return this.mobilePhone;
+      } else if (this.mainPhone) {
+        return this.mainPhone;
+      } else if (this.officePhone) {
+        return this.officePhone
+      } else {
+        return 'No Phone Available'
+      }
+    }
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#johnnyonit {
+    position: relative;
+    height: calc(100vh - 110px);
+    background: #238D77;
+    color: #fff;
+    padding: 20px;
+}
+#johnnyonit {
+    font-size: 24px;
+}
+
+#johnnyonit table {
+    width: unset;
+}
+
+#joi-agent {
+    position: absolute;
+    bottom: 20px;
+    left: 20px;
+}
+
+#joi-logo {
+    position: absolute;
+    bottom: 85px;
+    right: 20px;
+}
+
+#joi-agent-image {
+    width: 150px;
+    height: 150px;
+    border-radius: 100%;
+    background-size: cover;
+    background-position: center;
+    border: 3px solid #37E6F2;
 }
 </style>
